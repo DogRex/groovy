2e9f29a0473b50b28094e7898f335e813b2f2985
glaforge <glaforge>
2004-08-01 20:26:25 +0000

    Empty plugin and directory structure

---
+++ groovy/ide/groovyj/src/java/org/codehaus/groovy/intellij/GroovyJPlugin.java
@@ -0,0 +1,20 @@
+package org.codehaus.groovy.intellij;
+
+import com.intellij.openapi.components.ApplicationComponent;
+
+public class GroovyJPlugin implements ApplicationComponent
+{
+
+    public String getComponentName() {
+        return "groovyj.plugin";
+    }
+
+    public void initComponent() {
+        
+    }
+
+    public void disposeComponent() {
+        
+    }
+    
+}
