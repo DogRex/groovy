edb34aa64baf8b9172193c27716c5eb4564072ff
fraz <fraz>
2005-03-31 19:42:13 +0000

    GROOVY-619: Implemented PairedBraceMatcher for matching parentheses, brackets, and braces

---
+++ groovy/ide/groovyj/src/java/org/codehaus/groovy/intellij/language/GroovyPairedBraceMatcher.java
@@ -0,0 +1,39 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2005 The Codehaus - http://groovy.codehaus.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+
+package org.codehaus.groovy.intellij.language;
+
+import com.intellij.lang.BracePair;
+import com.intellij.lang.PairedBraceMatcher;
+
+import org.codehaus.groovy.antlr.parser.GroovyTokenTypes;
+
+import org.codehaus.groovy.intellij.psi.GroovyTokenTypeToElementTypeMappings;
+
+public class GroovyPairedBraceMatcher implements PairedBraceMatcher {
+
+    private static final BracePair[] BRACE_PAIRS = new BracePair[] {
+        new BracePair('(', GroovyTokenTypeToElementTypeMappings.getType(GroovyTokenTypes.LPAREN), ')', GroovyTokenTypeToElementTypeMappings.getType(GroovyTokenTypes.RPAREN), false),
+        new BracePair('[', GroovyTokenTypeToElementTypeMappings.getType(GroovyTokenTypes.LBRACK), ']', GroovyTokenTypeToElementTypeMappings.getType(GroovyTokenTypes.RBRACK), false),
+        new BracePair('{', GroovyTokenTypeToElementTypeMappings.getType(GroovyTokenTypes.LCURLY), '}', GroovyTokenTypeToElementTypeMappings.getType(GroovyTokenTypes.RCURLY), true)
+    };
+
+    public BracePair[] getPairs() {
+        return BRACE_PAIRS;
+    }
+}
