23a93b8d2262234a1c0105162d0c85f5f8e65650
zohar <zohar>
2005-05-02 11:47:53 +0000

    changed deprectaed partitioner class class - now inline with 3.1 api

--- groovy/ide/groovy-eclipse/GroovyEclipse/src/org/codehaus/groovy/eclipse/editor/GroovyDocumentSetupParticipant.java
+++ groovy/ide/groovy-eclipse/GroovyEclipse/src/org/codehaus/groovy/eclipse/editor/GroovyDocumentSetupParticipant.java
@@ -5,7 +5,7 @@ import org.eclipse.core.filebuffers.IDocumentSetupParticipant;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IDocumentExtension3;
 import org.eclipse.jface.text.IDocumentPartitioner;
-import org.eclipse.jface.text.rules.DefaultPartitioner;
+import org.eclipse.jface.text.rules.FastPartitioner;
 
 /**
  */
@@ -22,7 +22,7 @@ public class GroovyDocumentSetupParticipant  implements IDocumentSetupParticipan
 	public void setup(IDocument document) {
 		if (document instanceof IDocumentExtension3) {
 			IDocumentExtension3 extension3= (IDocumentExtension3) document;
-			IDocumentPartitioner partitioner= new DefaultPartitioner(GroovyPlugin.getPlugin().getGroovyPartitionScanner(), GroovyPartitionScanner.GROOVY_PARTITION_TYPES);
+			IDocumentPartitioner partitioner= new FastPartitioner(GroovyPlugin.getPlugin().getGroovyPartitionScanner(), GroovyPartitionScanner.GROOVY_PARTITION_TYPES);
 			extension3.setDocumentPartitioner(GroovyPlugin.GROOVY_PARTITIONING, partitioner);
 			partitioner.connect(document);
 		}
