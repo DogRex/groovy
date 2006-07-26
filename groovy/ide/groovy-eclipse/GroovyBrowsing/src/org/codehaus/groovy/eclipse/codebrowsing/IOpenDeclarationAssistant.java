71a203ef09f119809b949c96a209811b3a1ffbc4
emp <emp>
2006-07-26 05:33:19 +0000

    Initial commit. Stubs, that is all.

---
+++ groovy/ide/groovy-eclipse/GroovyBrowsing/src/org/codehaus/groovy/eclipse/codebrowsing/IOpenDeclarationAssistant.java
@@ -0,0 +1,5 @@
+package org.codehaus.groovy.eclipse.codebrowsing;
+
+public interface IOpenDeclarationAssistant {
+
+}
---
+++ groovy/ide/groovy-eclipse/GroovyBrowsing/src/org/codehaus/groovy/eclipse/codebrowsing/actions/OpenDeclaration.java
@@ -0,0 +1,16 @@
+package org.codehaus.groovy.eclipse.codebrowsing.actions;
+
+import org.codehaus.groovy.eclipse.editor.actions.EditingAction;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.text.BadLocationException;
+
+/**
+ * Starts the attempt to open the declaration of the identifier under the caret.  
+ * @author emp
+ */
+public class OpenDeclaration extends EditingAction {
+	@Override
+	public void doAction(IAction action) throws BadLocationException {
+		super.doAction(action);
+	}
+}
