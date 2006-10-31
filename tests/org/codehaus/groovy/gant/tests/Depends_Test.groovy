9b158c79723770c0dd039d1f36ab7301e345fa05
russel <russel>
2006-10-31 19:20:57 +0000

    Changed licence from LGPL to ASL 2.0.

--- tests/org/codehaus/groovy/gant/tests/Depends_Test.groovy
+++ tests/org/codehaus/groovy/gant/tests/Depends_Test.groovy
@@ -1,18 +1,16 @@
 //  Gant -- A Groovy build tool based on scripting Ant tasks
 //
-//  Copyright (C) 2006 Russel Winder <russel@russel.org.uk>
+//  Copyright © 2006 Russel Winder <russel@russel.org.uk>
 //
-//  This library is free software; you can redistribute it and/or modify it under the terms of
-//  the GNU Lesser General Public License as published by the Free Software Foundation; either
-//  version 2.1 of the License, or (at your option) any later version.
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+//  compliance with the License. You may obtain a copy of the License at
 //
-//  This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-//  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-//  See the GNU Lesser General Public License for more details.
+//    http://www.apache.org/licenses/LICENSE-2.0
 //
-//  You should have received a copy of the GNU Lesser General Public License along with this
-//  library; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
-//  Boston, MA 02110-1301 USA
+//  Unless required by applicable law or agreed to in writing, software distributed under the License is
+//  distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+//  implied. See the License for the specific language governing permissions and limitations under the
+//  License.
 
 package org.codehaus.groovy.gant.tests
 
@@ -22,8 +20,8 @@ import org.codehaus.groovy.gant.infrastructure.Gant
  *  A test for the depends processing, i.e. make sure the depends calls the method when appropriate and not
  *  when appropriate.
  *
- *  @author Russel Winder
- *  @version $Revision: 4090 $ $Date: 2006-10-01 16:38:57 +0100 (Sun, 01 Oct 2006) $
+ *  @author Russel Winder <russel@russel.org.uk>
+ *  @version $Revision$ $Date$
  */
 final class Depends_Test extends GantTestCase {
   void testNone ( ) {
