7cfa6f086d917491cc732c1553c4d353c7acd97a
emp <emp>
2006-12-17 11:01:01 +0000

    Added a guard around input setting code preventing Eclipse from sometimes going bezerk with an endless stream of error dialogs. Commented out boilerplate code for some other time, no useless buttons visible now.

--- groovy/ide/groovy-eclipse/org.codehaus.groovy.eclipse.astviews/src/org/codehaus/groovy/eclipse/astviews/ASTView.java
+++ groovy/ide/groovy-eclipse/org.codehaus.groovy.eclipse.astviews/src/org/codehaus/groovy/eclipse/astviews/ASTView.java
@@ -45,32 +45,25 @@ import org.eclipse.ui.part.DrillDownAdapter;
 import org.eclipse.ui.part.ViewPart;
 
 /**
- * This sample class demonstrates how to plug-in a new workbench view. The view
- * shows data obtained from the model. The sample creates a dummy model on the
- * fly, but a real implementation would connect to the model available either in
- * this or another plug-in (e.g. the workspace). The view is connected to the
- * model using a content provider.
- * <p>
- * The view uses a label provider to define how model objects should be
- * presented in the view. Each view can present the same model objects using
- * different labels and icons, if needed. Alternatively, a single label provider
- * can be shared between views in order to ensure that objects of the same type
- * are presented in the same way everywhere.
- * <p>
+ * A view into the Groovy AST. Anyone who needs to manipulate the AST will find
+ * this useful for exploring various nodes.
  */
-
 public class ASTView extends ViewPart { // implements ISelectionListener {
 	private TreeViewer viewer;
 
 	private DrillDownAdapter drillDownAdapter;
 
-	private Action action1;
-
-	private Action action2;
+//	private Action action1;
+//
+//	private Action action2;
 
 	private Action doubleClickAction;
 
 	private GroovyEditor editor;
+
+	private IPartListener partListener;
+
+	private GroovyBuildListener groovyBuildListener;
 
 	class ViewContentProvider implements IStructuredContentProvider, ITreeContentProvider {
 		ITreeNode root;
@@ -83,7 +76,7 @@ public class ASTView extends ViewPart { // implements ISelectionListener {
 
 		public Object[] getElements(Object inputElement) {
 			IFile file = (IFile) inputElement;
-			
+
 			root = TreeNodeFactory.createTreeNode(null, GroovyModel.getModel().getModuleNodes(file), "Module Nodes");
 
 			Object[] children = root.getChildren();
@@ -134,15 +127,20 @@ public class ASTView extends ViewPart { // implements ISelectionListener {
 		viewer.setLabelProvider(new ViewLabelProvider());
 		viewer.setSorter(null);
 		viewer.setInput(null);
-		makeActions();
-		hookContextMenu();
+//		makeActions();
+//		hookContextMenu();
 		hookDoubleClickAction();
 		hookGroovy();
-		contributeToActionBars();
+//		contributeToActionBars();
+	}
+
+	public void dispose() {
+		unhookGroovy();
+		super.dispose();
 	}
 
 	private void hookGroovy() {
-		IPartListener listener = new IPartListener() {
+		partListener = new IPartListener() {
 			public void partActivated(IWorkbenchPart part) {
 			}
 
@@ -153,7 +151,15 @@ public class ASTView extends ViewPart { // implements ISelectionListener {
 					return;
 				}
 				editor = null;
-				viewer.setInput(null);
+				// This is a guard - the content provider should not be null,
+				// but sometimes this happens when the
+				// part is disposed of for various reasons (unhandled exceptions
+				// AFAIK). Without this guard,
+				// error message popups continue until Eclipse if forcefully
+				// killed.
+				if (viewer.getContentProvider() != null) {
+					viewer.setInput(null);
+				}
 			}
 
 			public void partClosed(IWorkbenchPart part) {
@@ -165,17 +171,15 @@ public class ASTView extends ViewPart { // implements ISelectionListener {
 			public void partOpened(IWorkbenchPart part) {
 			}
 		};
-
-		getSite().getPage().addPartListener(listener);
+		getSite().getPage().addPartListener(partListener);
 
 		// Warm the listener up.
 		if (getSite().getPage().getActiveEditor() instanceof GroovyEditor) {
-			listener.partBroughtToTop(getSite().getPage().getActiveEditor());
+			partListener.partBroughtToTop(getSite().getPage().getActiveEditor());
 		}
 
 		// Listen to when the build changes.
-		GroovyModel model = GroovyModel.getModel();
-		model.addBuildListener(new GroovyBuildListener() {
+		groovyBuildListener = new GroovyBuildListener() {
 			public void fileBuilt(final IFile file) {
 				// HACK ALERT:
 				// fileBuilt is notified regardless of errors. However nothing
@@ -185,7 +189,8 @@ public class ASTView extends ViewPart { // implements ISelectionListener {
 				// These are set as a workspace operation, so this method
 				// will also have to do so, else it will check for markers
 				// before they have had a chance to be set.
-				// Note that for some reason markers are not always set, so this is not reliable.
+				// Note that for some reason markers are not always set, so this
+				// is not reliable.
 				try {
 					ResourcesPlugin.getWorkspace().run(new IWorkspaceRunnable() {
 						public void run(IProgressMonitor monitor) throws CoreException {
@@ -218,100 +223,107 @@ public class ASTView extends ViewPart { // implements ISelectionListener {
 					GroovyPlugin.getPlugin().logException(e.getMessage(), e);
 				}
 			}
-		});
-	}
-
-	private void hookContextMenu() {
-		MenuManager menuMgr = new MenuManager("#PopupMenu");
-		menuMgr.setRemoveAllWhenShown(true);
-		menuMgr.addMenuListener(new IMenuListener() {
-			public void menuAboutToShow(IMenuManager manager) {
-				ASTView.this.fillContextMenu(manager);
-			}
-		});
-		Menu menu = menuMgr.createContextMenu(viewer.getControl());
-		viewer.getControl().setMenu(menu);
-		getSite().registerContextMenu(menuMgr, viewer);
-	}
-
-	private void contributeToActionBars() {
-		IActionBars bars = getViewSite().getActionBars();
-		fillLocalPullDown(bars.getMenuManager());
-		fillLocalToolBar(bars.getToolBarManager());
-	}
-
-	private void fillLocalPullDown(IMenuManager manager) {
-		manager.add(action1);
-		manager.add(new Separator());
-		manager.add(action2);
-	}
-
-	private void fillContextMenu(IMenuManager manager) {
-		manager.add(action1);
-		manager.add(action2);
-		manager.add(new Separator());
-		drillDownAdapter.addNavigationActions(manager);
-		// Other plug-ins can contribute there actions here
-		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
-	}
-
-	private void fillLocalToolBar(IToolBarManager manager) {
-		manager.add(action1);
-		manager.add(action2);
-		manager.add(new Separator());
-		drillDownAdapter.addNavigationActions(manager);
-	}
-
-	private void makeActions() {
-		action1 = new Action() {
-			public void run() {
-				showMessage("Action 1 executed");
-			}
 		};
-		action1.setText("Action 1");
-		action1.setToolTipText("Action 1 tooltip");
-		action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(
-				ISharedImages.IMG_OBJS_INFO_TSK));
 
-		action2 = new Action() {
-			public void run() {
-				showMessage("Action 2 executed");
-			}
-		};
-		action2.setText("Action 2");
-		action2.setToolTipText("Action 2 tooltip");
-		action2.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(
-				ISharedImages.IMG_OBJS_INFO_TSK));
-		doubleClickAction = new Action() {
-			public void run() {
-				ISelection selection = viewer.getSelection();
-				Object obj = ((IStructuredSelection) selection).getFirstElement();
-				if (obj == null) {
-					return;
-				}
-				if (((ITreeNode) obj).getValue() instanceof ASTNode) {
-					Object value = ((ITreeNode) obj).getValue();
-					if (!(value instanceof ASTNode)) {
-						return;
-					}
-					
-					ASTNode node = (ASTNode) value;
-					if (node.getLineNumber() != -1) {
-						EditorPartFacade facade = new EditorPartFacade(editor);
-						try {
-							int offset0 = facade.getOffset(node.getLineNumber() - 1, node.getColumnNumber() - 1);
-							int offset1 = facade
-									.getOffset(node.getLastLineNumber() - 1, node.getLastColumnNumber() - 1);
-							// editor.setHighlightRange(offset0, offset1 -
-							// offset0, true);
-							facade.select(offset0, offset1 - offset0);
-						} catch (BadLocationException e) {
-						}
-					}
-				}
-			}
-		};
+		GroovyModel.getModel().addBuildListener(groovyBuildListener);
 	}
+
+	private void unhookGroovy() {
+		GroovyModel.getModel().removeBuildListener(groovyBuildListener);
+		getSite().getPage().removePartListener(partListener);
+	}
+
+//	private void hookContextMenu() {
+//		MenuManager menuMgr = new MenuManager("#PopupMenu");
+//		menuMgr.setRemoveAllWhenShown(true);
+//		menuMgr.addMenuListener(new IMenuListener() {
+//			public void menuAboutToShow(IMenuManager manager) {
+//				ASTView.this.fillContextMenu(manager);
+//			}
+//		});
+//		Menu menu = menuMgr.createContextMenu(viewer.getControl());
+//		viewer.getControl().setMenu(menu);
+//		getSite().registerContextMenu(menuMgr, viewer);
+//	}
+//
+//	private void contributeToActionBars() {
+//		IActionBars bars = getViewSite().getActionBars();
+//		fillLocalPullDown(bars.getMenuManager());
+//		fillLocalToolBar(bars.getToolBarManager());
+//	}
+//
+//	private void fillLocalPullDown(IMenuManager manager) {
+//		manager.add(action1);
+//		manager.add(new Separator());
+//		manager.add(action2);
+//	}
+//
+//	private void fillContextMenu(IMenuManager manager) {
+//		manager.add(action1);
+//		manager.add(action2);
+//		manager.add(new Separator());
+//		drillDownAdapter.addNavigationActions(manager);
+//		// Other plug-ins can contribute there actions here
+//		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
+//	}
+//
+//	private void fillLocalToolBar(IToolBarManager manager) {
+//		manager.add(action1);
+//		manager.add(action2);
+//		manager.add(new Separator());
+//		drillDownAdapter.addNavigationActions(manager);
+//	}
+
+//	private void makeActions() {
+//		action1 = new Action() {
+//			public void run() {
+//				showMessage("Action 1 executed");
+//			}
+//		};
+//		action1.setText("Action 1");
+//		action1.setToolTipText("Action 1 tooltip");
+//		action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(
+//				ISharedImages.IMG_OBJS_INFO_TSK));
+//
+//		action2 = new Action() {
+//			public void run() {
+//				showMessage("Action 2 executed");
+//			}
+//		};
+//		action2.setText("Action 2");
+//		action2.setToolTipText("Action 2 tooltip");
+//		action2.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(
+//				ISharedImages.IMG_OBJS_INFO_TSK));
+//		doubleClickAction = new Action() {
+//			public void run() {
+//				ISelection selection = viewer.getSelection();
+//				Object obj = ((IStructuredSelection) selection).getFirstElement();
+//				if (obj == null) {
+//					return;
+//				}
+//				if (((ITreeNode) obj).getValue() instanceof ASTNode) {
+//					Object value = ((ITreeNode) obj).getValue();
+//					if (!(value instanceof ASTNode)) {
+//						return;
+//					}
+//
+//					ASTNode node = (ASTNode) value;
+//					if (node.getLineNumber() != -1) {
+//						EditorPartFacade facade = new EditorPartFacade(editor);
+//						try {
+//							int offset0 = facade.getOffset(node.getLineNumber() - 1, node.getColumnNumber() - 1);
+//							int offset1 = facade
+//									.getOffset(node.getLastLineNumber() - 1, node.getLastColumnNumber() - 1);
+//							// editor.setHighlightRange(offset0, offset1 -
+//							// offset0, true);
+//							facade.select(offset0, offset1 - offset0);
+//						} catch (BadLocationException e) {
+//						}
+//					}
+//				}
+//			}
+//		};
+//	}
 
 	private void hookDoubleClickAction() {
 		viewer.addDoubleClickListener(new IDoubleClickListener() {
